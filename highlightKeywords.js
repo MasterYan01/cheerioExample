9d984f95cedabc8f50d5d3fbe182a7595e02a63f
Ethan <ethanyan@castec.com.tw>
2024-09-04 11:47:41 +0800

    更新

---
+++ highlightKeywords.js
@@ -0,0 +1,13 @@
+// highlightKeywords.js
+
+function highlightKeywords(title, chalk) {
+  return title
+      .replace(/鴻海/g, (match) => chalk.red(match))
+      .replace(/台積電/g, (match) => chalk.red(match))
+      .replace(/AI/g, (match) => chalk.yellow(match))
+      .replace(/廣達/g, (match) => chalk.green(match))
+      .replace(/緯創/g, (match) => chalk.green(match))
+      .replace(/輝達/g, (match) => chalk.green(match));
+}
+
+module.exports = highlightKeywords;
